5e5b35739b8153f5ff9f20b480fd0d1e518b3aa0
Dach-Coin <roman_starch@mail.ru>
2021-04-12 20:45:20 +0300

    Исправил ошибку

--- other/sql_queries/adaptive_maintainse_plan/Адаптивный дефраг индексов и апдейт статистик по всем базам.sql
+++ other/sql_queries/adaptive_maintainse_plan/Адаптивный дефраг индексов и апдейт статистик по всем базам.sql
@@ -32,7 +32,7 @@ BEGIN
 		 , @outputResults = 0 /* 1 = output fragmentation information; 0 = do not output */
 		 , @debugMode = 0 /* display some useful comments to help determine if/where issues occur; 1 = display debug comments; 0 = do not display debug comments*/
 		 , @timeLimit = @limitMinutes
-		 , @dbScope = 'ERP_demo' 
+		 , @dbScope = @DBname 
 		 , @forceRescan = 1   /* Whether to force a rescan of indexes into the tbl_AdaptiveIndexDefrag_Working table or not; 1 = force, 0 = use existing scan when available, used to continue where previous run left off */
 		 , @defragDelay = '00:00:05'
 		 , @minFragmentation = 5
