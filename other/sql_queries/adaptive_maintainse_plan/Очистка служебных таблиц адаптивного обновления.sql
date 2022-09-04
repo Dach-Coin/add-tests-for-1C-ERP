5f880a0cbb41d2047984bd73a05201c0cfae3243
Dach-Coin <roman_starch@mail.ru>
2022-09-04 21:37:20 +0300

    Добавил скрипт очистки служебных таблиц адаптивного обновления

---
+++ other/sql_queries/adaptive_maintainse_plan/Очистка служебных таблиц адаптивного обновления.sql
@@ -0,0 +1,7 @@
+TRUNCATE TABLE tbl_AdaptiveIndexDefrag_Analysis_log
+TRUNCATE TABLE tbl_AdaptiveIndexDefrag_Exceptions
+TRUNCATE TABLE tbl_AdaptiveIndexDefrag_IxDisableStatus
+TRUNCATE TABLE tbl_AdaptiveIndexDefrag_log
+TRUNCATE TABLE tbl_AdaptiveIndexDefrag_Stats_log
+TRUNCATE TABLE tbl_AdaptiveIndexDefrag_Stats_Working
+TRUNCATE TABLE tbl_AdaptiveIndexDefrag_Working
